3ef65b88049bcc333b9bac00484c5ab3ae8de35e
Aman Arora <aman.arora@paytm.com>
2021-06-10 17:45:16 +0530

    asdbk

--- grpc-client/src/main/java/com/grpc/grpcistio/client/HelloController.java
+++ grpc-client/src/main/java/com/grpc/grpcistio/client/HelloController.java
@@ -2,7 +2,12 @@ package com.grpc.grpcistio.client;
 
 import com.grpc.grpcistio.grpcprotos.HelloWorldService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,14 +22,27 @@ public class HelloController {
 	private GrpcClientConfiguration grpcClientConfiguration;
 
 	@GetMapping("/hello")
-	public String hello(@RequestParam(name = "name", defaultValue = "Aman", required = false) String name) {
+	public String hello(@RequestParam(name = "name", defaultValue = "Aman", required = false) String name, @RequestHeader("mydata") String data) {
+		System.out.println("Got data in header field mydata: " + data);
 		HelloWorldService.HelloRequest request = HelloWorldService.HelloRequest
 				.newBuilder()
 				.setName(name)
 				.build();
 
 		HelloWorldService.HelloResponse response = grpcClientConfiguration.getStub().sayHello(request);
-		System.out.println("Server response received: "+ response.getMessage());
+		System.out.println("Server response received: " + response.getMessage());
 		return response.getMessage();
+	}
+
+	@PostMapping("/edge")
+	public ResponseEntity<String> edge(@RequestHeader("method") String method, @RequestHeader("path") String path, @RequestHeader("authority") String authority) {
+		System.out.println("Got headers in edge function " + method + " " +path + " " + authority);
+
+		HttpHeaders responseHeaders = new HttpHeaders();
+		responseHeaders.set("set-mydata",
+				"this is a sample header with data");
+
+		return ResponseEntity.noContent()
+				.headers(responseHeaders).build();
 	}
 }
